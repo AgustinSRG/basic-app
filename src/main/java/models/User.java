6a6ff1a3b5f4d2364ffe7e315b338789b799c797
AgustinSRG <41982624+AgustinSRG@users.noreply.github.com>
2019-04-15 13:31:43 +0200

    Initial Commit

---
+++ src/main/java/models/User.java
@@ -0,0 +1,11 @@
+package models;
+
+public class User {
+
+		private java.lang.String id;
+ 
+		public java.lang.String getId() {
+			return this.id;
+		}
+}
+
---
+++ static/js/lib/dashboard.js
@@ -0,0 +1,23 @@
+$(function () {
+
+    /*$(".grid-stack").gridstack( {staticGrid: true} );
+
+
+    /*$(window).on('resize', function(){
+        widgets = $('.grid-stack').children("div");
+
+        for (var i = 0; i < widgets.length; i++) {
+            var grafico = widgets[i].childNodes[0].childNodes[1].childNodes[0].__chartist__;
+            panel = widgets[i].childNodes[0];
+            var altura = panel.offsetHeight - 50;
+
+            //grafico.update(0, {height: altura });
+        }
+
+    });*/
+
+    //Workspace.grid = $(".grid-stack").data("gridstack");
+
+    //Workspace.loadGrid();
+
+});
