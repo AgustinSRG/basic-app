b8df4c118d74e15bb9183243182ab16a77aece04
AgustinSRG <41982624+AgustinSRG@users.noreply.github.com>
2019-04-25 12:45:07 +0200

    Set star functionality

--- src/main/java/controllers/UserStarController.java
+++ src/main/java/controllers/UserStarController.java
@@ -2,6 +2,7 @@ package controllers;
 
 import java.util.Map;
 
+import org.springframework.web.bind.annotation.CookieValue;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -9,6 +10,8 @@ import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import messages.UserStarMessage;
+import models.TextFile;
+import models.User;
 import models.UserStar;
 
 @RestController
@@ -28,7 +31,42 @@ public class UserStarController extends GeneralController {
 	}
 	
 	@PostMapping("/star/set")
-	public UserStarMessage setStar(@RequestBody Map<String,Object> body) {
-		return null;
+	public UserStarMessage setStar(@RequestBody Map<String,Object> body, @CookieValue("session") String sessionCookie) {
+		String fileId = null;
+		boolean star = false;
+		
+		try {
+			fileId = body.get("file").toString();
+			star = body.get("star").toString().equalsIgnoreCase("yes");
+		} catch (Exception ex) {}
+		
+		TextFile f = server.find(TextFile.class).where().eq("identifier", fileId).findOne();
+		if (f == null) {
+			return new UserStarMessage(false);
+		}
+		
+		User user = this.findUserBySessionCookie(sessionCookie);
+		
+		if (user == null) {
+			return new UserStarMessage(false);
+		}
+		
+		UserStar starO = server.find(UserStar.class).where().eq("user", user.getID()).eq("text_file", fileId).findOne();
+		
+		if (starO == null) {
+			starO = new UserStar();
+			starO.setTextfile(f.getID());
+			starO.setUser(user.getID());
+		}
+
+		starO.setStar(star);
+		
+		try {
+			starO.save();
+		} catch (Exception ex) {
+			ex.printStackTrace();
+		}
+		
+		return new UserStarMessage(star);
 	}
 }
